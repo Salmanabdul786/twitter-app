27909c3129c7d2f6f54de8468e74df2cee4447e7
Abdul Kareem <41697424+Salmanabdul786@users.noreply.github.com>
2024-07-27 01:35:55 +0530

    backend complete

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,12 @@ import {v2 as cloudinary} from 'cloudinary';
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from './routes/user.routes.js'
 import postRoutes from './routes/post.routes.js'
+import notificationRoutes from './routes/notification.route.js'
 import connectDB from "./db/connectdb.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
+
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 	api_key: process.env.CLOUDINARY_API_KEY,
@@ -21,10 +23,10 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notification", notificationRoutes);
 
-app.get('/',(req,res)=>{
-    res.send("Server");
-});
+
+
 app.listen(8000, () => {
   console.log(`Running on ${PORT}`);
   connectDB();
