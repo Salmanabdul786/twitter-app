cf369328f9b0a52b8a17d756a687b3667af751b9
Abdul Kareem <41697424+Salmanabdul786@users.noreply.github.com>
2024-07-28 02:26:53 +0530

    changes

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -98,7 +98,10 @@ export const likeUnlikePost=async(req, res)=>{
                 //umlike
                 await Post.updateOne({_id:postId}, { $pull: { likes: userId } });
                 await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } });
-                res.status(200).json({message:"Post Unliked"})
+
+
+			const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString());
+			res.status(200).json(updatedLikes);
         }else{
             post.likes.push(userId);
             await User.updateOne({ _id: userId }, { $push: { likedPosts: postId } });
@@ -109,7 +112,8 @@ export const likeUnlikePost=async(req, res)=>{
 				type: "like",
 			});
 			await notification.save();
-            res.status(200).json({ message: "Post Liked successfully" });
+            const updatedLikes = post.likes;
+			res.status(200).json(updatedLikes);
         }
     }
     catch (error) {
