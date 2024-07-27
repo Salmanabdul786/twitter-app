894babbe5da9a7046bcb5526a8752e275a0da39a
Abdul Kareem <41697424+Salmanabdul786@users.noreply.github.com>
2024-07-28 01:27:29 +0530

    post create

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ cloudinary.config({
 });
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(express.json());
+app.use(express.json({limit:"20mb"}));
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
